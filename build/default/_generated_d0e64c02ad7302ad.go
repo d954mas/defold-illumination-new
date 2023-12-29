1caeb66d62900cbd65c4b2720873fe1eea969848
d954mas <d954mas@gmail.com>
2023-12-29 10:35:58 +0300

    update

---
+++ build/default/_generated_d0e64c02ad7302ad.go
@@ -0,0 +1,17 @@
+components {
+  id: "fps"
+  component: "/show_dt/show_dt.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  property_decls {
+  }
+}
