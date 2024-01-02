daf805d8b757aa2595161cc8355eb1a8fdb652b1
d954mas <d954mas@gmail.com>
2024-01-02 15:47:51 +0300

    fixed xmath error

--- xmath/src/xMath.cpp
+++ xmath/src/xMath.cpp
@@ -672,7 +672,7 @@ static void xMathLuaInit(lua_State* L)
 static dmExtension::Result xMathInitialize(dmExtension::Params* params)
 {
     xMathLuaInit(params->m_L);
-    dmLogError("Registered %s Extension\n", MODULE_NAME);
+    dmLogInfo("Registered %s Extension\n", MODULE_NAME);
     return dmExtension::RESULT_OK;
 }
 
